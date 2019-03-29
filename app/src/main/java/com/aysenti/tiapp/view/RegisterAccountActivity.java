9bdc6bfbf65fda765671d16fa149de2ff8140e4f
Cristopher Gonzalez <cagv1992@gmail.com>
2019-03-29 10:42:48 -0300

    Creacion de registro de usuarios y configuracion de material design, init toolbar

--- app/src/main/java/com/aysenti/tiapp/view/RegisterAccountActivity.java
+++ app/src/main/java/com/aysenti/tiapp/view/RegisterAccountActivity.java
@@ -1,7 +1,10 @@
 package com.aysenti.tiapp.view;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.aysenti.tiapp.R;
 
@@ -11,6 +14,7 @@ public class RegisterAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+        //showToolbar(getResources().getString(R.string.to));
     }
 
     @Override
@@ -33,5 +37,6 @@ public class RegisterAccountActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
     }
+
 
 }
