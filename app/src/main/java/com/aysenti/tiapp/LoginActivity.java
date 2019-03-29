def7abaa17b7947e2b28284934cebd5814795ce8
Cristopher Gonzalez <cagv1992@gmail.com>
2019-03-29 14:17:38 -0300

    Finalizacion de toolbar y intent entre login y registro de usuario

--- app/src/main/java/com/aysenti/tiapp/LoginActivity.java
+++ app/src/main/java/com/aysenti/tiapp/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.aysenti.tiapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.aysenti.tiapp.view.RegisterAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-    public void goCreateAccount(View view){}
+    public void goCreateAccount(View view){
+        //Puede ser igual LoginActivity.this en el primer parametro
+        Intent intent = new Intent(this, RegisterAccountActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/aysenti/tiapp/view/RegisterAccountActivity.java
+++ app/src/main/java/com/aysenti/tiapp/view/RegisterAccountActivity.java
@@ -14,7 +14,7 @@ public class RegisterAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
-        showToolbar(getResources().getString(R.string.title_activity_register_account),false);
+        showToolbar(getResources().getString(R.string.title_activity_register_account),true);
     }
 
     public void showToolbar(String tittle, boolean upButton){
