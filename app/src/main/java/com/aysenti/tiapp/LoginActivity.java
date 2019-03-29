0c31975cc679b5a2305ed249d3b93d74fed0eeec
Cristopher Gonzalez <cagv1992@gmail.com>
2019-03-28 22:24:00 -0300

    Configuracion de Material design en textos, botones y estilos en general

--- app/src/main/java/com/aysenti/tiapp/LoginActivity.java
+++ app/src/main/java/com/aysenti/tiapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.aysenti.tiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +11,5 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void goCreateAccount(View view){}
 }
