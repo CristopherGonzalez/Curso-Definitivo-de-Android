27e1cc0f18dfb1171596ac9adf6a62752cc12db3
Cristopher Gonzalez <cagv1992@gmail.com>
2019-04-08 00:13:04 -0400

    Creacion de fragment y enlace con bottom bar, se termina cardview de home y se agregan nuevos iconos

--- app/src/main/java/com/aysenti/tiapp/view/ContainerActivity.java
+++ app/src/main/java/com/aysenti/tiapp/view/ContainerActivity.java
@@ -1,9 +1,20 @@
 package com.aysenti.tiapp.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.aysenti.tiapp.R;
+import com.aysenti.tiapp.view.fragment.DetailsFragment;
+import com.aysenti.tiapp.view.fragment.HomeFragment;
+import com.aysenti.tiapp.view.fragment.SearchFragment;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -11,5 +22,42 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+        BottomNavigationView bottomBar = findViewById(R.id.bottom_nav_view);
+        HomeFragment homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, homeFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .addToBackStack(null).commit();
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId()==R.id.opt_home){
+                    HomeFragment homeFragment = new HomeFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, homeFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .addToBackStack(null).commit();
+                }else if(menuItem.getItemId()==R.id.opt_details){
+                    DetailsFragment detailFragment = new DetailsFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, detailFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .addToBackStack(null).commit();
+                }else if(menuItem.getItemId()==R.id.opt_search){
+                    SearchFragment searchFragment = new SearchFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, searchFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .addToBackStack(null).commit();
+                }
+                return false;
+            }
+        });
+        /*FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+
     }
 }
