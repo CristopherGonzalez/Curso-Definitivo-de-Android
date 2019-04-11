b6317394d3b71bc0d19e91c8679342d56058c9ad
Cristopher Gonzalez <cagv1992@gmail.com>
2019-04-10 23:54:05 -0400

    Se agrega efectos de transiciones a las imagenes usadas en el cardview y algunos fragment

--- app/src/main/java/com/aysenti/tiapp/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/aysenti/tiapp/adapter/PictureAdapterRecyclerView.java
@@ -1,9 +1,13 @@
 package com.aysenti.tiapp.adapter;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Explode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,7 +50,16 @@ public class PictureAdapterRecyclerView  extends RecyclerView.Adapter<PictureAda
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(activity, PictureDetailActivity.class);
-                activity.startActivity(intent);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    Explode explode = new Explode();
+                    explode.setDuration(2000);
+                    activity.getWindow().setExitTransition(explode);
+                    activity.startActivity(intent,
+                            ActivityOptionsCompat.makeSceneTransitionAnimation(activity, v,activity.getString(R.string.transition_image)).toBundle());
+                }else{
+                    activity.startActivity(intent);
+                }
             }
         });
 
